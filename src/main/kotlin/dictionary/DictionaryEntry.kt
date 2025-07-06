06faf386d53b33095b7dfe27cc74c6a560bc433c
Andreas Volkmann <anvolkma@microsoft.com>
2025-07-05 18:15:44 -0700

    rest

---
+++ src/main/kotlin/dictionary/DictionaryEntry.kt
@@ -0,0 +1,9 @@
+package dictionary
+
+data class DictionaryEntry(
+    val timeGauge: Int,
+    val phraseOffset: Int,
+    val keycodeOffset: Int,
+    val phrase: String,
+    val keycodes: List<String>
+)
